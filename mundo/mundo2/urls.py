0c9a8fb85a1290edd34253c421bdaa49389bcbaf
Lizromans <lizethserna09@gmail.com>
2025-03-19 17:23:16 -0500

    revisión

--- mundo/mundo2/urls.py
+++ mundo/mundo2/urls.py
@@ -2,7 +2,6 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('nosotros', views.nosotros, name='nosotros'),
     path('bienvenido', views.bienvenido, name='bienvenido'),
+    path('registro', views.registro, name='registro'),
 ]
--- mundo/mundo2/views.py
+++ mundo/mundo2/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-
-def inicio(request):
-    return HttpResponse("<h1>prueba de formulario</h1>")
-
-def nosotros(request):
-    return render(request, 'paginas/nosotros.html')
-
 def bienvenido(request):
     return render(request,'paginas/bienvenido.html')
+
+
+def registro(request):
+    return render(request, 'paginas/registro.html')
